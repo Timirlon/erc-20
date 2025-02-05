bde190207deadddbce62d6e933c9ee15b4a34963
Timirlon <tturgimbayev@gmail.com>
2025-02-05 11:45:32 +0500

    part-two commit

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract ErcAssignmentToken is ERC20 {
+contract MyToken is ERC20 {
     address private owner;
 
     // Mapping to store transaction data
